be64323f3f8dad9766985bb585347fb9a7360bcc
Reynell Quevedo <reynell4587@hotmail.com>
2023-02-14 11:54:47 -0500

    Food microservice initialized

---
+++ food-court-service/src/main/java/com/pragma/foodcourtservice/FoodCourtServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.foodcourtservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodCourtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodCourtServiceApplication.class, args);
+	}
+
+}
---
+++ food-court-service/src/test/java/com/pragma/foodcourtservice/FoodCourtServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.foodcourtservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodCourtServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
