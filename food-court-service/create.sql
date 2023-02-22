b4b67d1ea2db0a19f454e49d0cb84871f010f3f6
Reynell Quevedo <reynell4587@hotmail.com>
2023-02-22 16:45:48 -0500

    Updating .gitignore

--- food-court-service/create.sql
+++ food-court-service/create.sql
@@ -29,7 +29,7 @@ create table foodPlate (id int8 not null, activo boolean default true, descripci
 create table restaurant (id int8 not null, direccion varchar(255), id_propietario int8, nit int8, nombre varchar(255), telefono varchar(255), url_logo varchar(255), primary key (id));
 alter table if exists foodPlate add constraint FKgxx76oqdq4ausvp6hspn57shv foreign key (id_categoria) references categoria;
 alter table if exists foodPlate add constraint FKdm75cs23t4omy649bpy3a052c foreign key (id_restaurante) references restaurant;
-create sequence hibernate_sequence start 1 increment 1;
+create sequence hibernate_sequence starat 1 increment 1;
 create table categoria (id int8 not null, descripcion varchar(255), nombre varchar(255), primary key (id));
 create table foodPlate (id int8 not null, activo boolean default true, descripcion varchar(255), id_categoria int8, id_restaurante int8, nombre varchar(255), precio int8, url_imagen varchar(255), primary key (id));
 create table restaurant (id int8 not null, direccion varchar(255), id_propietario int8, nit int8, nombre varchar(255), telefono varchar(255), url_logo varchar(255), primary key (id));
