a7b4cb92cde060bdccd8c837d571005c81f99ee1
Reynell Quevedo <reynell4587@hotmail.com>
2023-02-25 07:50:18 -0500

    Began the food-court tests.

---
+++ food-court-service/src/test/java/com/pragma/foodcourtservice/domain/api/IFoodPlateServicePortTest.java
@@ -0,0 +1,24 @@
+package com.pragma.foodcourtservice.domain.api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IFoodPlateServicePortTest {
+
+    @Test
+    void saveFoodPlate() {
+    }
+
+    @Test
+    void updateFoodPlate() {
+    }
+
+    @Test
+    void changeStateFoodPlate() {
+    }
+
+    @Test
+    void listTheFoodPlatesByCategory() {
+    }
+}
